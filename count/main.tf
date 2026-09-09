003ceb8cf553260c1505416ad79fcdea4c22e91c
Avinash-DevopsEr <avi.mk1998@gmail.com>
2026-09-10 02:06:58 +0530

    Add instance_count variable and update EC2 instance count configuration

--- count/main.tf
+++ count/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2"{
     ami = "ami-01a00762f46d584a1"
     instance_type = "t3.micro"
     key_name = "mumbai"
-    count = 2
+    count = var.instance_count
     tags = {
         Name = "server-${count.index+1}"
     }
